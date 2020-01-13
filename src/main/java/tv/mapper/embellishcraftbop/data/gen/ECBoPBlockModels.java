339466f0103461d1a9e6aa52f6d90495ce4091ed
Mapper <killermapper@outlook.com>
2020-01-13 23:38:08 +0100

    Still working on data generators.

--- src/main/java/tv/mapper/embellishcraftbop/data/gen/ECBoPBlockModels.java
+++ src/main/java/tv/mapper/embellishcraftbop/data/gen/ECBoPBlockModels.java
@@ -2,6 +2,7 @@ package tv.mapper.embellishcraftbop.data.gen;
 
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockModelProvider;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import tv.mapper.embellishcraftbop.EmbellishCraftBOP;
@@ -18,7 +19,7 @@ public class ECBoPBlockModels extends BlockModelProvider
     @Override
     public String getName()
     {
-        return "EmbellishCraft BoP Block Models";
+        return "EmbellishCraft-BoP Block Models";
     }
 
     @Override
@@ -30,7 +31,71 @@ public class ECBoPBlockModels extends BlockModelProvider
         {
             name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
             wood = name.replace("_chair", "");
-            getBuilder(name).parent(getExistingFile(mcLoc("block/cube_all"))).texture("all", "biomesoplenty:block/" + wood + "_planks");
+            getBuilder(name).parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/chair"))).texture("1",
+                new ResourceLocation("biomesoplenty", "block/" + wood + "_planks")).texture("particle", new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+        }
+
+        for(Block block : ECBoPBlocks.TERRACE_CHAIRS)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            wood = name.replace("_terrace_chair", "");
+            getBuilder(name).parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/terrace_chair"))).texture("material", modLoc("block/" + wood + "_terrace_table")).texture(
+                "particle", modLoc("block/" + wood + "_terrace_table"));
+        }
+
+        for(Block block : ECBoPBlocks.TABLES)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            wood = name.replace("_table", "");
+            getBuilder(name + "_foot").parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/table/table_foot"))).texture("material",
+                new ResourceLocation("biomesoplenty", "block/" + wood + "_planks")).texture("particle", new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+            getBuilder(name + "_inventory").parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/table/table_inventory"))).texture("material",
+                new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+            getBuilder(name + "_top").parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/table/table_top"))).texture("material",
+                new ResourceLocation("biomesoplenty", "block/" + wood + "_planks")).texture("particle", new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+        }
+
+        for(Block block : ECBoPBlocks.TERRACE_TABLES)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            getBuilder(name).parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/terrace_table"))).texture("material", modLoc("block/" + name)).texture("particle",
+                modLoc("block/" + name));
+        }
+
+        for(Block block : ECBoPBlocks.FANCY_TABLES)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            wood = name.replace("_fancy_table", "");
+            getBuilder(name + "_foot").parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/table/fancy/fancy_table_foot"))).texture("side",
+                modLoc("block/" + name + "_side")).texture("top", modLoc("block/" + name + "_top")).texture("particle", new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+            getBuilder(name + "_inventory").parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/table/fancy/fancy_table_inventory"))).texture("side",
+                modLoc("block/" + name + "_side")).texture("top", modLoc("block/" + name + "_top"));
+            getBuilder(name + "_top").parent(getExistingFile(new ResourceLocation("embellishcraft", "block/furniture/table/fancy/fancy_table_top"))).texture("side",
+                modLoc("block/" + name + "_side")).texture("top", modLoc("block/" + name + "_top")).texture("particle", new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+        }
+
+        for(Block block : ECBoPBlocks.FANCY_DOORS)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            getBuilder(name + "_bottom").parent(getExistingFile(mcLoc("block/door_bottom"))).texture("bottom", modLoc("block/" + name + "_bottom")).texture("top", modLoc("block/" + name + "_top"));
+            getBuilder(name + "_bottom_hinge").parent(getExistingFile(mcLoc("block/door_bottom_rh"))).texture("bottom", modLoc("block/" + name + "_bottom")).texture("top",
+                modLoc("block/" + name + "_top"));
+            getBuilder(name + "_top").parent(getExistingFile(mcLoc("block/door_top"))).texture("bottom", modLoc("block/" + name + "_bottom")).texture("top", modLoc("block/" + name + "_top"));
+            getBuilder(name + "_top_hinge").parent(getExistingFile(mcLoc("block/door_top_rh"))).texture("bottom", modLoc("block/" + name + "_bottom")).texture("top", modLoc("block/" + name + "_top"));
+        }
+
+        for(Block block : ECBoPBlocks.SUSPENDED_STAIRS)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            wood = name.replace("_suspended_stairs", "");
+            getBuilder(name).parent(getExistingFile(new ResourceLocation("embellishcraft", "block/stairs/base/suspended_stairs"))).texture("all",
+                new ResourceLocation("biomesoplenty", "block/" + wood + "_planks"));
+        }
+
+        for(Block block : ECBoPBlocks.CRATES)
+        {
+            name = block.getRegistryName().toString().replace("embellishcraft-bop:", "");
+            getBuilder(name).parent(getExistingFile(mcLoc("block/cube_all"))).texture("all", modLoc("block/" + name));
         }
     }
 }
--- src/main/java/tv/mapper/embellishcraftbop/data/gen/ECBoPGenerators.java
+++ src/main/java/tv/mapper/embellishcraftbop/data/gen/ECBoPGenerators.java
@@ -1,6 +1,11 @@
 package tv.mapper.embellishcraftbop.data.gen;
 
+import java.util.Collections;
+
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourcePackType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -12,8 +17,23 @@ public class ECBoPGenerators
     public static void gatherData(GatherDataEvent event)
     {
         DataGenerator generator = event.getGenerator();
+        ExistingFileHelper delegate = event.getExistingFileHelper();
+
+        ExistingFileHelper helper = new ExistingFileHelper(Collections.emptyList(), true)
+        {
+            @Override
+            public boolean exists(ResourceLocation loc, ResourcePackType type, String pathSuffix, String pathPrefix)
+            {
+                if(loc.getNamespace() == "embellishcraft" || loc.getNamespace() == "biomesoplenty")
+                {
+                    return true;
+                }
+                return delegate.exists(loc, type, pathSuffix, pathPrefix);
+            }
+        }; // Temporary to counter the crash when generator can't find files from dependencies
+
         generator.addProvider(new ECBoPRecipes(generator));
         generator.addProvider(new ECBoPLootTables(generator));
-        generator.addProvider(new ECBoPBlockModels(generator, event.getExistingFileHelper()));
+        generator.addProvider(new ECBoPBlockModels(generator, helper));
     }
 }
